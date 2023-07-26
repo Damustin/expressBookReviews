154d146f2f7d0a7e5a5817ef56387a55ddea868b
Damustin <damianamlp@hotmail.com>
2023-07-25 22:57:17 -0300

    npm files and first block code for coursera

--- router/general.js
+++ router/general.js
@@ -12,8 +12,18 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const booksList = Object.values(books);
+
+  const booksHTML = booksList.map((book) => {
+    return `<div>
+              <h2>${book.title}</h2>
+              <p>${book.description}</p>
+            </div>`;
+  });
+
+  const htmlResponse = `<h1>Lista de Libros</h1>${booksHTML.join('')}`;
+
+  return res.status(200).send(htmlResponse);
 });
 
 // Get book details based on ISBN
@@ -22,16 +32,33 @@ public_users.get('/isbn/:isbn',function (req, res) {
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const booksList = Object.values(books);
+
+  const booksByAuthor = booksList.filter((book) => book.author === author);
+
+  if (booksByAuthor.length === 0) {
+    return res.status(404).json({ message: "Autor no encontrado." });
+  }
+
+  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const title = req.params.title;
+  const booksList = Object.values(books);
+
+  const bookbytitle = booksList.filter((book) => book.title === title);
+
+  if (bookbytitle.length === 0){
+    return res.status(404).json({"error":"no ta" });
+      
+  }
+  return res.status(200).json(bookbytitle);
 });
 
 //  Get book review
