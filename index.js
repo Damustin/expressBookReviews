786e4f65a0be3143f107eafcece2ebb5d5394e3b
Damustin <damianamlp@hotmail.com>
2023-08-08 17:16:39 -0300

    up date in general.js

--- index.js
+++ index.js
@@ -13,7 +13,70 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 app.use("/customer/auth/*", function auth(req, res, next) {
   // Implementar el mecanismo de autenticación aquí
   // ...
+  //   // Verificar si existe un token de acceso almacenado en la sesión
+  const token = req.session.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Acceso no autorizado. Token no proporcionado." });
+  }
+
+  try {
+    // Verificar el token usando la clave secreta para decodificarlo
+    const decodedToken = jwt.verify(token, 'your_secret_key');
+
+    // Almacenar los datos del usuario en la solicitud para su uso posterior si es necesario
+    req.user = decodedToken;
+
+    // Llamar a la siguiente función en la cadena de middleware
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido o expirado." });
+  }
 });
+
+app.post("/customer/login", (req, res) => {
+  // Aquí debes validar el usuario y contraseña proporcionados en la solicitud
+  const { username, password } = req.body;
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: "Credenciales inválidas. Acceso no autorizado." });
+  }
+
+  // Si las credenciales son válidas, generamos un token con la información del usuario
+  const token = jwt.sign({ username: user.username, role: user.role }, 'your_secret_key');
+
+  // Almacenar el token en la sesión del cliente
+  req.session.token = token;
+
+  return res.status(200).json({ message: "Inicio de sesión exitoso.", token });
+});
+
+app.post("/customer/login", (req, res) => {
+  // Aquí debes validar el usuario y contraseña proporcionados en la solicitud
+  const { username, password } = req.body;
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: "Credenciales inválidas. Acceso no autorizado." });
+  }
+
+  // Si las credenciales son válidas, generamos un token con la información del usuario
+  const token = jwt.sign({ username: user.username, role: user.role }, 'your_secret_key');
+
+  // Almacenar el token en la sesión del cliente
+  req.session.token = token;
+
+  return res.status(200).json({ message: "Inicio de sesión exitoso.", token });
+});
+
+
+
+
+
+
+
+
 
 const PORT = 5000;
 
@@ -21,4 +84,7 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT, () => console.log("Server is running"));
+
+
+
 
--- router/general.js
+++ router/general.js
@@ -5,13 +5,13 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   const booksList = Object.values(books);
 
   const booksHTML = booksList.map((book) => {
@@ -27,12 +27,23 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "El libro no esta" });
+  } else {
+    return res.status(200).json(book);
+  }
+}
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
- public_users.get('/author/:author', function (req, res) {
+);
+
+
+
+
+
+public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   const booksList = Object.values(books);
 
@@ -46,7 +57,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   //Write your code here
 
   const title = req.params.title;
@@ -54,17 +65,25 @@ public_users.get('/title/:title',function (req, res) {
 
   const bookbytitle = booksList.filter((book) => book.title === title);
 
-  if (bookbytitle.length === 0){
-    return res.status(404).json({"error":"no ta" });
-      
+  if (bookbytitle.length === 0) {
+    return res.status(404).json({ "error": "no ta" });
+
   }
   return res.status(200).json(bookbytitle);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({message:'book not exist'})
+  } else {
+    return res.status(200).json(book.reviewa);
+  }
+  
+}
+);
 
 module.exports.general = public_users;
