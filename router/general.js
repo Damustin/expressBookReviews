fb84f020bb52484cf5a5f9cf8530f79609ef9901
Damustin <damianamlp@hotmail.com>
2023-08-08 17:58:27 -0300

    new update

--- router/general.js
+++ router/general.js
@@ -11,34 +11,41 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  const booksList = Object.values(books);
+public_users.get('/', async function (req, res) {
+  try {
+    const booksList = Object.values(books);
 
-  const booksHTML = booksList.map((book) => {
-    return `<div>
-              <h2>${book.title}</h2>
-              <p>${book.description}</p>
-            </div>`;
-  });
+    const booksHTML = booksList.map((book) => {
+      return `<div>
+                <h2>${book.title}</h2>
+                <p>${book.description}</p>
+              </div>`;
+    });
 
-  const htmlResponse = `<h1>Lista de Libros</h1>${booksHTML.join('')}`;
+    const htmlResponse = `<h1>Lista de Libros</h1>${booksHTML.join('')}`;
 
-  return res.status(200).send(htmlResponse);
+    return res.status(200).send(htmlResponse);
+  } catch (error) {
+    return res.status(500).json({ message: "Error en el servidor." });
+  }
 });
 
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
-  if (!book) {
-    return res.status(404).json({ message: "El libro no esta" });
-  } else {
-    return res.status(200).json(book);
-  }
-}
-  //Write your code here
-);
 
+  try {
+    if (!book) {
+      return res.status(404).json({ message: "El libro no está." });
+    } else {
+      return res.status(200).json(book);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error en el servidor." });
+  }
+});
 
 
 
@@ -75,15 +82,15 @@ public_users.get('/title/:title', function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn];
+  const book = Object.values(books);
+  const bookrev = book.filter((book)=>book.isbn === isbn);
 
-  if (!book) {
-    return res.status(404).json({message:'book not exist'})
-  } else {
-    return res.status(200).json(book.reviewa);
+  if (!bookrev) {
+    return res.status(404).json({ message: 'book not exist' })
   }
+  return res.status(200).send(bookrev.reviews)
+    
+  });
   
-}
-);
 
 module.exports.general = public_users;
